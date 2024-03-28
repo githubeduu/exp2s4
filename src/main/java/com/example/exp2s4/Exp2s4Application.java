e5d59709575acaeabfad7c6db76abbb4df7459e2
Eduardo <edo.carvajal@hotmail.com>
2024-03-28 01:59:43 -0300

    first commit

---
+++ src/main/java/com/example/exp2s4/Exp2s4Application.java
@@ -0,0 +1,13 @@
+package com.example.exp2s4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exp2s4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exp2s4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exp2s4/Exp2s4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exp2s4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exp2s4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
